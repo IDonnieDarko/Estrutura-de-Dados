32fba32c4ca48e3f8e95641daa33566ba9d98e92
Sávio Fernandes <118401296+IDonnieDarko@users.noreply.github.com>
2023-08-14 09:42:51 -0300

    Add files via upload

--- atividade1/verifica_numero_primo.c
+++ atividade1/verifica_numero_primo.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int is_prime(int num) {
     if (num <= 1) {
-        return 0; // Não é primo
+        return 0; // Números menores ou iguais a 1 não são primos
     }
 
-    for (int i = 2; i * i <= num; ++i) {
+    for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) {
-            return 0; // Não é primo
+            return 0; // Se encontrar um divisor, não é primo
         }
     }
 
-    return 1; // É primo
+    return 1; // Se nenhum divisor for encontrado, é primo
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Uso: %s <numero>\n", argv[0]);
-        return 1;
-    }
+int main() {
+    int numero;
+    printf("Digite um número: ");
+    scanf("%d", &numero);
 
-    int num = atoi(argv[1]);
-
-    if (is_prime(num)) {
-        printf("1 - O numero %d e primo.\n", num);
+    if (is_prime(numero)) {
+        printf("1\n"); // Imprime 1 para número primo
     } else {
-        printf("0 - O numero %d nao e primo.\n", num);
+        printf("0\n"); // Imprime 0 para número não primo
     }
 
     return 0;
